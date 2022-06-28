7245c53351207e9f58a46ca57255036505ccb3b3
LeonMesquita <leonmesquita16@gmail.com>
2022-06-27 22:58:08 -0300

    criação do projeto

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import LoginScreen from "./LoginScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+export default function App(){
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginScreen />}/>
+        </Routes>
+        </BrowserRouter>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import  ReactDOM from "react-dom";
+import App from "./components/App";
+import './style.css';
+ReactDOM.render(<App />, document.querySelector(".root"))
